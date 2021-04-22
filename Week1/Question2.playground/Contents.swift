0c6a09a915685f52ca40b955e13b2c99efd0a69e
Oğuzhan Bekir <oguzhanbekir53@gmail.com>
2021-04-22 22:24:50 +0300

    Update Contents.swift

--- Week1/Question2.playground/Contents.swift
+++ Week1/Question2.playground/Contents.swift
@@ -3,9 +3,6 @@
 //1, 2, 3, 5, 8, 13, 21, 34, 55, 89, ...
 //By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.
 
-
-
-
 import UIKit
 
 func evenFibonacciNumbers(max: Int) -> Int {
