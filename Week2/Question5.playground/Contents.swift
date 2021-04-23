c6c914e7c0c78f9b73ef3f4432d07a5a0902e68d
Oğuzhan Bekir <oguzhanbekir53@gmail.com>
2021-04-23 22:28:57 +0300

    Update Contents.swift

--- Week2/Question5.playground/Contents.swift
+++ Week2/Question5.playground/Contents.swift
@@ -1,6 +1,7 @@
 //2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
 //What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 
+
 //To find the Greatest Common Divisor (GCD):
 //GCD(a, b) = GCD(b, a % b)
 
