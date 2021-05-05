ee68d66eb4171c87b2a4524d4da303048fe74b60
Oguzhan Bekir <oguzhanbekir53@gmail.com>
2021-05-05 22:00:19 +0300

    initial commit

--- Week4/NotificationListener/NotificationListener/NotificationListenerVC.swift
+++ Week4/NotificationListener/NotificationListener/NotificationListenerVC.swift
@@ -8,22 +8,20 @@
 import UIKit
 
 class NotificationListenerVC: UIViewController {
-
+    
+    var notificationData: [String:Any] = [:]
+    @IBOutlet weak var sendDataTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func sendButtonPressed(_ sender: Any) {
+        notificationData["data"] = sendDataTextField.text
+        NotificationCenter.default.post(name: .sendDataNotification, object: nil, userInfo: notificationData)
+        dismiss(animated: true, completion: nil)
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
--- Week4/NotificationListener/NotificationListener/ViewController.swift
+++ Week4/NotificationListener/NotificationListener/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var dataLabel: UILabel!
+    var notificationData: [String:Any] = [:]
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let notificationCenter: NotificationCenter = NotificationCenter.default
+        notificationCenter.addObserver(self, selector: #selector(handleData(notification:)), name: .sendDataNotification, object: nil)
     }
-
-
+    
+    @objc func handleData(notification:Notification) {
+        if let name = notification.userInfo?["data"] as? String {
+            dataLabel.text = name
+        }
+    }
 }
 
+extension Notification.Name {
+    static let sendDataNotification = Notification.Name(rawValue: "sendData")
+}
