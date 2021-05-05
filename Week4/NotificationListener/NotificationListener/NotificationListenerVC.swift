bb9d9025b415060ff3aa2476c7dd81471e358013
Oguzhan Bekir <oguzhanbekir53@gmail.com>
2021-05-05 21:59:26 +0300

    initial commit

---
+++ Week4/NotificationListener/NotificationListener/NotificationListenerVC.swift
@@ -0,0 +1,29 @@
+//
+//  NotificationListenerVC.swift
+//  NotificationListener
+//
+//  Created by Oguzhan Bekir on 5.05.2021.
+//
+
+import UIKit
+
+class NotificationListenerVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Week4/NotificationListener/NotificationListener/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NotificationListener
+//
+//  Created by Oguzhan Bekir on 5.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
